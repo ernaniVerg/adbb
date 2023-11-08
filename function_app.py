89d621c698992c203240a6058b946c413d7b7726
Ernani Vergniano <34281172+ernaniVerg@users.noreply.github.com>
2023-11-08 12:40:40 -0300

    at2

--- function_app.py
+++ function_app.py
@@ -5,5 +5,6 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="req")
 def main(req: func.HttpRequest) -> str:
-    user = req.params.get("user")
-    return f"Hello, {user}!"
+    #user = req.params.get("user")
+    #return f"Hello, {user}!"
+    return "Hello, man!"
