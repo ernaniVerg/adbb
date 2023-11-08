16bf6583e44ab2579ec1a6f631c43cd017a1eba1
Ernani Vergniano <34281172+ernaniVerg@users.noreply.github.com>
2023-11-08 16:58:59 -0300

    pyspark

--- function_app.py
+++ function_app.py
@@ -1,10 +1,16 @@
 import azure.functions as func
+import pandas as pd
+import numpy
+import pyspark.pandas as ps
+from pyspark.sql import SparkSession
+
 
 app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="req")
+
 def main(req: func.HttpRequest) -> str:
-    #user = req.params.get("user")
-    #return f"Hello, {user}!"
-    return "Hello, man ok!"
+    user = req.params.get("text")
+    return f"Hello, {text}!"
+    #return "Hello, man ok!"
