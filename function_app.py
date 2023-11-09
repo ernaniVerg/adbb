b3e0e34250414a0f4d094f4a9c2c9c83b2484a37
Ernani Vergniano <34281172+ernaniVerg@users.noreply.github.com>
2023-11-09 09:44:33 -0300

    return

--- function_app.py
+++ function_app.py
@@ -6,32 +6,12 @@ import pyspark.pandas as ps
 from pyspark.sql import SparkSession
 
 
-#app = func.FunctionApp()
-bp = func.Blueprint() 
-#@app.function_name(name="HttpTrigger1")
-#@app.route(route="req")
-@bp.route(route="default_template")
+app = func.FunctionApp()
 
-def default_template(req: func.HttpRequest) -> func.HttpResponse: 
-    logging.info('Python HTTP trigger function processed a request.') 
+@app.function_name(name="HttpTrigger1")
+@app.route(route="req")
 
-    name = req.params.get('text') 
-    if not name: 
-        try: 
-            req_body = req.get_json() 
-        except ValueError: 
-            pass 
-        else: 
-            name = req_body.get('text') 
-
-    if name: 
-        return func.HttpResponse( 
-            f"Hello, {name}. This HTTP-triggered function " 
-            f"executed successfully.") 
-    else: 
-        return func.HttpResponse( 
-            "This HTTP-triggered function executed successfully. " 
-            "Pass a name in the query string or in the request body for a" 
-            " personalized response.", 
-            status_code=200 
-        ) 
+def main(req: func.HttpRequest) -> str:
+    user = req.params.get("text")
+    return f"Hello, {user}!"
+    #return "Hello, man ok!"
