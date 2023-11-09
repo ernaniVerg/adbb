4a12810687902378c3ecd081a8bd6d3e5b263013
Ernani Vergniano <34281172+ernaniVerg@users.noreply.github.com>
2023-11-09 14:35:37 -0300

    at

--- function_app.py
+++ function_app.py
@@ -18,5 +18,5 @@ def main(req: func.HttpRequest) -> str:
     filename = "data.zip"
     path, headers = urlretrieve(url, filename)
     ref = spark.read.csv(path,header = True)
-    return f"{ref}"
+    return ref.show()
     #return "Hello, man ok!"
